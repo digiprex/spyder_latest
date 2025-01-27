8cbc2379c51e948d8a54c2b52298e3fed0ea151e
Utkarsh <utkarsh.malviya@gkcfood.com>
2025-01-27 21:44:38 +0530

    Minor fixes

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -15,6 +15,41 @@ export class FormComponent {
     message: new FormControl(""),
   });
   public success: boolean = false;
+  
+  public links = [
+    {
+      "text": "Home",
+      "link": "/home"
+    },
+    {
+      "text": "About Us",
+      "link": "/about"
+    },
+    {
+      "text": "Pricing",
+      "link": "/pricing"
+    },
+    {
+      "text": "Testimonial",
+      "link": "/testimonial"
+    },
+    {
+      "text": "Faq",
+      "link": "/faq"
+    },
+    {
+      "text": "Blog",
+      "link": "/blog"
+    }
+  ];
+  public social = {
+    "email": "info@usespyder.com",
+    "phone": "+1 (970) 639-7833",
+    "address": "Bundl Financial Technologies Inc, 2055 Limestone Rd Ste 200-c Wilmington, De 19808",
+    "hrefEmail": "mailto:info@usespyder.com",
+    "hrefPhone": "tel: +1 (970) 639-7833",
+    "map": "https://maps.app.goo.gl/XRJyQmZvrqzTfYcD7"
+  };
 
   constructor(private appService: AppService) { }
 
